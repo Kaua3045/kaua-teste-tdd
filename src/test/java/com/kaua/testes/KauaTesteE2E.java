3f4f58814f23cdfce2509af0b782f40308ed8801
Kauã Pereira <zkauacodes@hotmail.com>
2023-02-10 18:20:27 -0300

    aqui funciona com o flyway

--- src/test/java/com/kaua/testes/KauaTesteE2E.java
+++ src/test/java/com/kaua/testes/KauaTesteE2E.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 //import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.MySQLContainer;
-//import org.testcontainers.containers.PostgreSQLContainer;
+//import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -21,17 +21,20 @@ import org.testcontainers.utility.DockerImageName;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class KauaTesteE2E {
 
-    @Container
-    public static final MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8"));
-
 //    @Container
-//    public static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14"));
+//    public static final MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8"));
 
-//    @DynamicPropertySource
-//    static void postgresDbProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-////        registry.add("spring.datasource.port", postgres::getJdbcUrl);
-//    }
+    @Container
+    public static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15"));
+
+    @DynamicPropertySource
+    static void postgresDbProperties(DynamicPropertyRegistry registry) {
+        postgres.start();
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+//        registry.add("spring.datasource.port", postgres::getJdbcUrl);
+    }
 //    @Container
 //    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
 
@@ -59,6 +62,6 @@ public class KauaTesteE2E {
 
     @Test
     public void testWorking() {
-        Assertions.assertTrue(mysql.isRunning());
+        Assertions.assertTrue(postgres.isRunning());
     }
 }
